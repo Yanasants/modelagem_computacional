82073c29e0fc03fcf55ffe933374e4cb1df93bab
Yana Santos <59098432+Yanasants@users.noreply.github.com>
2023-06-13 14:50:28 -0300

    trabalho final

--- edo.py
+++ edo.py
@@ -6,7 +6,7 @@ from funcoes_trabalho_final import *
 
 class Edo:
 
-  def __init__(self, y_inicial, dom, a, b, I_app, I_function, methods=[], output=pd.DataFrame([])):
+  def __init__(self, y_inicial, dom, a, b, I_app, I_function, v0, n0, m0, h0, fixo=False, methods=[], output=pd.DataFrame([])):
     self.y = y_inicial
     self.dom = dom
     self.a = a
@@ -15,12 +15,13 @@ class Edo:
     self.I_function = I_function 
     self.current_density = None
     self.methods = methods
+    self.fixo = fixo
 
     self.step = (self.b - self.a)/self.dom
     self.size = self.dom-1
     self.all_x = np.arange(self.a, self.b, self.step)
     self.V, self.n, self.m, self.h = np.zeros(len(self.all_x)), np.zeros(len(self.all_x)), np.zeros(len(self.all_x)), np.zeros(len(self.all_x))
-    self.V[0], self.n[0], self.m[0], self.h[0] = 0, 0, 0, 0
+    self.V[0], self.n[0], self.m[0], self.h[0] = v0, n0, m0, h0
 
     self.output = pd.DataFrame([])
     self.df_output()
@@ -39,7 +40,7 @@ class Edo:
       yp = self.all_y_adbash2[i] + self.h*(3*k1 - k2)/2
       self.all_y_adbash2[i+1] = self.all_y_adbash2[i] + 0.5*self.h*(k1 + self.f(self.all_x[i+1], yp))
   
-
+  # Alpha e Beta variando
   def rk4_step(self, V, n, m, h, dt, I_app):
 
     k1_V = dt * dV_dt(V, m, h, n, I_app)
@@ -69,19 +70,53 @@ class Edo:
 
     return V, n, m, h
   
+  # Alpha e Beta fixos 
+  def rk4_step_fixo(self, V, n, m, h, dt, I_app):
+
+    k1_V = dt * dV_dt(V, m, h, n, I_app)
+    k1_n = dt * dn_dt(V, n)
+    k1_m = dt * dm_params_fixo(V, m)
+    k1_h = dt * dh_params_fixo(V, h)
+    
+    k2_V = dt * dV_dt(V + 0.5 * k1_V, m, h, n, I_app)
+    k2_n = dt * dn_dt(V + 0.5 * dt, n + 0.5 * k1_n)
+    k2_m = dt * dm_params_fixo(V + 0.5 * dt, m + 0.5 * k1_m)
+    k2_h = dt * dh_params_fixo(V + 0.5 * dt, h + 0.5 * k1_h)
+
+    k3_V = dt * dV_dt(V + 0.5 * k2_V, m, h, n, I_app)
+    k3_n = dt * dn_dt(V + 0.5 * dt, n + 0.5 * k2_n)
+    k3_m = dt * dm_params_fixo(V + 0.5 * dt, m + 0.5 * k2_m)
+    k3_h = dt * dh_params_fixo(V + 0.5 * dt, h + 0.5 * k2_h)
+
+    k4_V = dt * dV_dt(V + k3_V, m, h, n, I_app)
+    k4_n = dt * dn_dt(V + dt, n + k3_n)
+    k4_m = dt * dm_params_fixo(V + dt, m + k3_m)
+    k4_h = dt * dh_params_fixo(V + dt, h + k3_h)
+
+    V += (k1_V + 2.0 * k2_V + 2.0 * k3_V + k4_V) / 6.0
+    n += (k1_n + 2.0 * k2_n + 2.0 * k3_n + k4_n) / 6.0
+    m += (k1_m + 2.0 * k2_m + 2.0 * k3_m + k4_m) / 6.0
+    h += (k1_h + 2.0 * k2_h + 2.0 * k3_h + k4_h) / 6.0
+
+    return V, n, m, h
+  
   def I_function_to_output(self):
     self.current_density = self.I_function(self.V, self.n, self.m, self.h)
   
   def df_output(self):
     self.output['Passo'] = self.all_x
     if 'rk4_final' in self.methods:
-      for i in range(1, len(self.all_x)):
-        self.V[i], self.n[i], self.m[i], self.h[i] = self.rk4_step(self.V[i-1], self.n[i-1], self.m[i-1], self.h[i-1], self.step, self.I_app)
+      if self.fixo:
+        for i in range(1, len(self.all_x)):
+          self.V[i], self.n[i], self.m[i], self.h[i] = self.rk4_step_fixo(self.V[i-1], self.n[i-1], self.m[i-1], self.h[i-1], self.step, self.I_app)
+      else:
+        for i in range(1, len(self.all_x)):
+          self.V[i], self.n[i], self.m[i], self.h[i] = self.rk4_step(self.V[i-1], self.n[i-1], self.m[i-1], self.h[i-1], self.step, self.I_app)    
+      self.I_function_to_output()
       self.output['V(t)'] = self.V
       self.output['n(t)'] = self.n
       self.output['m(t)'] = self.m
       self.output['h(t)'] = self.h
-      self.I_function_to_output()
       self.output['I(t)'] = self.current_density
 
     return self.output
--- funcoes_trabalho_final.py
+++ funcoes_trabalho_final.py
@@ -5,6 +5,11 @@ import plotly.graph_objects as go
 params = {"gk":36, "gna":120, "gl":0.3, "vk":12, "vna":-115, "vl":-10.613, "cm":1}
 # Gráfico 02
 params_2 = {"gk":24.27, "gna":120, "gl":0.3, "vk":12, "vna":-115, "vl":-10.613, "cm":1}
+# Gráfico 03
+params_3 = {"gk":15.3, "gna":120, "gl":0.3, "vk":12, "vna":-115, "vl":-10.613, "cm":1}
+
+
+
 
 # N PARAMS
 def alpha_n(V):
@@ -15,6 +20,11 @@ def beta_n(V):
     beta_n = 0.125*np.exp(V/80)
     return beta_n
 
+def dn_dt(V, n):
+    return alpha_n(V)*(1.0 - n) - beta_n(V)*n
+
+
+
 # M PARAMS
 def alpha_m(V):
     alpha_m = 0.1*(V + 25)/(np.exp((V + 25)/10) - 1)
@@ -23,6 +33,15 @@ def alpha_m(V):
 def beta_m(V):
     beta_m = 4*np.exp(V/18)
     return beta_m
+
+def dm_dt(V, m):
+    return alpha_m(V)*(1.0 - m) - beta_m(V)*m
+
+def dm_params_fixo(V, m):
+    return 3.82*(1.0 - m) - 0.15*m
+
+
+
 
 # H PARAMS
 def alpha_h(V):
@@ -33,28 +52,18 @@ def beta_h(V):
     beta_h = 1.0 / (np.exp((V + 30.0) / 10.0) + 1.0)
     return beta_h
 
-""" # CORRENTES
-def i_Na(V, m, h):
-    return params["gna"]*m**3 * h * (V - params["vna"])
+def dh_dt(V, h):
+    return alpha_h(V)*(1.0 - h) - beta_h(V)*h
 
-def i_K(V, n):
-    return params["gk"]* n**4 * (V - params["vk"])
+def dh_params_fixo(V, h):
+    return 1.19*h 
 
-def i_L(V):
-    return params["gl"]* (V - params["vl"]) """
+
+
 
 def dV_dt(V, m, h, n, I_app):
     return (I_app - params["gk"]*n**4*(V - params["vk"]) - params["gna"]*m**3*h* (V - params["vna"]) -  params["gl"]*(V - params["vl"]))/params["cm"]
 
-## Calculo da Equcao 3 com u e (n, m, h). Onde alpha_u = alpha(V) e Beta_u = Beta_u(V)
-def dn_dt(V, n):
-    return alpha_n(V)*(1.0 - n) - beta_n(V)*n
-
-def dm_dt(V, m):
-    return alpha_m(V)*(1.0 - m) - beta_m(V)*m
-
-def dh_dt(V, h):
-    return alpha_h(V)*(1.0 - h) - beta_h(V)*h
 
 def calc_current_density(V, m, h, n):
     # Ionica individual
@@ -65,6 +74,18 @@ def calc_current_density(V, m, h, n):
     # Calculo da Corrente Ionica Total
     I_total = I_Na + I_K + I_L
     return I_total
+
+
+def gk_function(V, m, n, h):
+    Ik = params["gk"]*(n**4)*(V - params["vk"])
+    gk = Ik/(V-params["vk"])*(n**4)
+    return gk
+
+def gna_function(V, m, n, h):
+    I_Na = params["gna"]*(m**3)*h*(V - params["vna"])
+    gna = I_Na/(V-params["vna"])*(m**3)*h
+    return gna
+
 
 def I_function(V, m, h, n, I_app):
     return params["cm"]* dV_dt(V, m, h, n, I_app) + calc_current_density(V, m, h, n)
@@ -80,5 +101,24 @@ def plot_output(variable=None, title=None, dfs=None):
             
     fig.update_layout(title_text=f'{title}', title_x=0.5,\
                     xaxis_title='t', yaxis_title=f'{variable}(t)',\
+                    height = 400, width = 600, font={'size':10})
+    fig.show()
+
+def plot_condutancias(variable=None, dfs=None):
+    fig = go.Figure()
+    for df_key in dfs.keys():
+        if variable=="gk":
+            var_k = gk_function(V=dfs[df_key].V, m=dfs[df_key].m, n=dfs[df_key].n, h=dfs[df_key].h)
+            element = "potássio"
+        elif variable=="gna":
+            var_k = gna_function(V=dfs[df_key].V, m=dfs[df_key].m, n=dfs[df_key].n, h=dfs[df_key].h)
+            element = "sódio"
+
+        fig.add_trace(go.Scatter(x=dfs[df_key].output['Passo'], y=var_k,
+                    mode='lines',
+                    name=f'{variable} {df_key}'))
+        
+    fig.update_layout(title_text=f'Condutância do {element}', title_x=0.5,\
+                    xaxis_title='t', yaxis_title=f'{variable} (t)',\
                     height = 400, width = 600, font={'size':10})
     fig.show()
